c54a6f498970b3911c570aa592be030b375f173f
lokesh b s <honecycombindin.lokesh@gmail.com>
2024-09-24 14:26:36 +0530

    in active carousel

--- src/pages/home/testimonials.js
+++ src/pages/home/testimonials.js
@@ -3,9 +3,30 @@ import React, { useState, useRef, useEffect } from "react";
 import Dummying from "../../assets/dummy/ssImage.png";
 import Image from "next/image";
 import Client from "../../assets/clients/clients1.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Testimonials = () => {
     const [videoModal, setVideoModal] = useState(false);
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
     const [videoLink, setVideoLink] = useState("");
     const [selectedTestiMonial, setSelectedTestimonial] = useState({
         logo: Client,
@@ -58,24 +79,77 @@ const Testimonials = () => {
             videoSrc:
                 "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
         },
+        {
+            logo: Client,
+            content:
+                "I’m very happy that I chose Cliniceo for my patient management software in my clinic. It has really changed my clinic’s working style. It’s made everything work very smoothly. Everything has been streamlined",
+            name: "Dr Ishan Sardesai",
+            place: "Delhi",
+            thumbNail: Dummying,
+            videoSrc:
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        },
+        {
+            logo: Client,
+            content:
+                "I’m very happy that I chose Cliniceo for my patient management software in my clinic. It has really changed my clinic’s working style. It’s made everything work very smoothly. Everything has been streamlined",
+            name: "Dr Ishan Sardesai",
+            place: "Banglore",
+            thumbNail: Dummying,
+            videoSrc:
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        },
+        {
+            logo: Client,
+            content:
+                "I’m very happy that I chose Cliniceo for my patient management software in my clinic. It has really changed my clinic’s working style. It’s made everything work very smoothly. Everything has been streamlined",
+            name: "Dr Ishan Sardesai",
+            place: "Kolkata",
+            thumbNail: Dummying,
+            videoSrc:
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        },
+        {
+            logo: Client,
+            content:
+                "I’m very happy that I chose Cliniceo for my patient management software in my clinic. It has really changed my clinic’s working style. It’s made everything work very smoothly. Everything has been streamlined",
+            name: "Dr Ishan Sardesai",
+            place: "Mumbai",
+            thumbNail: Dummying,
+            videoSrc:
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        },
     ];
 
     const ActiveCards = ({ data }) => {
+        const [fadeState, setFadeState] = useState("fade-in"); // Controls the fade state
+
+        useEffect(() => {
+            // Trigger fade-out when data changes, then fade-in
+            setFadeState("fade-out");
+
+            const timeoutId = setTimeout(() => {
+                setFadeState("fade-in");
+            }, 300); // Adjust the timing to match your CSS transition duration
+
+            return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount
+        }, [data]);
+
         return (
-            <div className="border border-[#D9D9D9]  rounded-[15px] w-[100%] flex flex-wrap  ">
-                <div className="w-[100%] sm:w-[66%] overflow-clip  relative">
-                    <Image className="object-cover w-[100%] " src={data?.thumbNail} />
+            <div
+                className={`border border-[#D9D9D9] rounded-[15px] w-[100%] flex flex-wrap  `}
+            >
+                <div className="w-[100%] sm:w-[66%] overflow-clip relative">
+                    <Image className="   object-cover w-[100%] " src={data?.thumbNail} />
                     <div
                         onClick={() => {
-                            setVideoLink(
-                                data?.videoSrc
-                            );
+                            setVideoLink(data?.videoSrc);
                             setVideoModal(!videoModal);
                         }}
                         className="absolute top-1/2 left-1/2 -translate-x-1/2 group"
                     >
                         <svg
-                            className="group-hover:stroke-[#1C75BC] last:cursor-pointer"
+                            className="group-hover:stroke-[#1C75BC] last:cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out"
                             width="60"
                             height="60"
                             viewBox="0 0 30 30"
@@ -86,36 +160,29 @@ const Testimonials = () => {
                                 d="M14.962 27.5c6.904 0 12.5 -5.596 12.5 -12.5S21.867 2.5 14.962 2.5 2.462 8.097 2.462 15s5.597 12.5 12.5 12.5"
                                 stroke="white"
                                 className="group-hover:stroke-[#1C75BC]"
-                                stroke-width="1.9047619047619047"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="1.9047619047619047"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                             <path
-                                d="M10.925 15.287V13.2c0 -2.6 1.837 -3.663 4.087 -2.363l1.813 1.05 1.812 1.05c2.25 1.3 2.25 3.425 0 4.725l-1.812 1.05 -1.813 1.05c-2.25 1.3 -4.087 0.238 -4.087 -2.362z"
+                                d="M10.925 15.287V13.2c0 -2.6 1.837 -3.663 4.087 -2.363l1.813 1.05 1.812 1.05c2.25 1.3 2.25 3.425 0 4.725l-1.812 1.05-1.813 1.05c-2.25 1.3-4.087 0.238-4.087-2.362z"
                                 stroke="white"
-                                stroke-width="1.9047619047619047"
-                                stroke-miterlimit="10"
+                                strokeWidth="1.9047619047619047"
+                                strokeMiterlimit="10"
                                 className="group-hover:stroke-[#1C75BC]"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                         </svg>
                     </div>
                 </div>
                 <div className="w-[100%] sm:w-[33%] sm:gap-10 gap-5 flex flex-col px-6 sm:px-16 justify-evenly">
-
-
-                    <div className="flex items-center justify-center w-full my-6 ">
+                    <div className="flex items-center justify-center w-full my-6">
                         <Image width={150} src={Client} />
                     </div>
                     <div className="scale-100 font-poppins">
-                        <p>
-                            {data?.content}
-                        </p>
+                        <p>{data?.content}</p>
                     </div>
-
-
-
                     <div className="text-left font-poppins py-6">
                         <strong className="font-600 font-poppins">{data?.name}</strong>
                         <p className="font-light">{data?.place}</p>
@@ -128,10 +195,10 @@ const Testimonials = () => {
         return (
             <div className="border border-[#D9D9D9]  rounded-[15px] w-[100%]  ">
                 <div className="w-[100%] aspect-video relative">
-                    <Image className="object-cover" src={data.thumbNail} />
+                    <Image className="object-cover " src={data.thumbNail} />
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 group">
                         <svg
-                            className="group-hover:stroke-[#1C75BC] last:cursor-pointer"
+                            className="group-hover:stroke-[#1C75BC] last:cursor-pointer  hover:scale-110 transition-all duration-300 ease-in-out"
                             width="30"
                             height="30"
                             viewBox="0 0 30 30"
@@ -179,13 +246,13 @@ const Testimonials = () => {
         return (
             <>
                 {open && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-0 ">
+                    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-0 px-2 ">
                         {/* Modal Container */}
-                        <div className="relative bg-white rounded-lg overflow-hidden aspect-video w-full">
+                        <div className="relative bg-white rounded-lg overflow-hidden aspect-video ">
                             {/* Close Button */}
                             <button
                                 onClick={onCLose}
-                                className="fixed top-10 right-10 text-gray-700 text-lg hover:text-black"
+                                className="absolute top-10 right-10 text-gray-700 text-lg hover:text-black z-50"
                             >
                                 <svg
                                     width="48"
@@ -204,10 +271,10 @@ const Testimonials = () => {
                                 </svg>
                             </button>
 
-                            <div className="w-full aspect-video  z-50">
+                            <div className=" flex items-center justify-center z-50">
                                 <video
                                     controls
-                                    className="w-full h-full"
+                                    className="aspect-video"
                                     autoPlay
                                     src={videoSrc}
                                 >
@@ -223,20 +290,28 @@ const Testimonials = () => {
     };
 
     return (
-        <div className="container mx-auto px-4  scale-100 sm:scale-[75%] ">
-            <Title title={"Testimonials"} />
-            <div className="my-12  ">
-                <ActiveCards data={selectedTestiMonial} />
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-8 items-center justify-center  my-12">
-                {testiMonialsObj.slice(1).map((items, index) => {
-                    return (
-                        <div onClick={() => setSelectedTestimonial(items)} key={index}>
-                            <InActiveCards data={items} />
-                        </div>
-                    );
-                })}
+        <div>
+            <div className="container mx-auto px-4  scale-100 sm:scale-[75%] ">
+                <Title title={"Testimonials"} />
+                <div className="my-12  ">
+                    <ActiveCards data={selectedTestiMonial} />
+                </div>
 
+                <div>
+                    <Carousel responsive={responsive} className={"h-[550px]"}>
+                        {testiMonialsObj.slice(1).map((items, index) => {
+                            return (
+                                <div
+                                    onClick={() => setSelectedTestimonial(items)}
+                                    key={index}
+                                    className="px-3"
+                                >
+                                    <InActiveCards data={items} />
+                                </div>
+                            );
+                        })}
+                    </Carousel>
+                </div>
             </div>
             <VideoModal
                 videoSrc={videoLink}
